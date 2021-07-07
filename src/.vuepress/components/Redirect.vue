cc63d0b9d4b1d3137f26ee67bbb81152a9a3c286
Nightscape Coffee <23473065+coffeebank@users.noreply.github.com>
2021-07-07 15:43:57 -0700

    Move import styles

--- src/.vuepress/components/Redirect.vue
+++ src/.vuepress/components/Redirect.vue
@@ -19,3 +19,7 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+@import '../styles/index.styl';
+</style>
--- src/.vuepress/components/Status/BotCount.vue
+++ src/.vuepress/components/Status/BotCount.vue
@@ -69,3 +69,7 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+@import '../../styles/index.styl';
+</style>
