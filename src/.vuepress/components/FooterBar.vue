8cd88a84ad68de857781f3dcf3aa7603b61eb48b
Nightscape Coffee <23473065+coffeebank@users.noreply.github.com>
2022-05-07 01:48:30 -0700

    Update footer

--- src/.vuepress/components/FooterBar.vue
+++ src/.vuepress/components/FooterBar.vue
@@ -2,6 +2,8 @@
   <div class="bg-gray-100 min-h-5r">
     <div class="theme-default-content content__default">
       <router-link to="/legal">Terms of Use&ensp;&middot;&ensp;Privacy Policy</router-link>
+      &ensp;&middot;&ensp;
+      Icons by <a href="https://icons8.com/" rel="noopener noreferrer">Icons8</a>
     </div>
   </div>
 </template>
