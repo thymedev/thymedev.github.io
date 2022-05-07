df8412bd28e332ebf26c05e56c11dba422f7c949
Nightscape Coffee <23473065+coffeebank@users.noreply.github.com>
2022-05-07 02:08:37 -0700

    Update footer

--- src/.vuepress/components/FooterBar.vue
+++ src/.vuepress/components/FooterBar.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="bg-gray-100 min-h-5r">
+  <div class="bg-gray-100 text-gray-400 min-h-5r">
     <div class="theme-default-content content__default">
-      <router-link to="/legal">Terms of Use&ensp;&middot;&ensp;Privacy Policy</router-link>
-      &ensp;&middot;&ensp;
-      <span class="text-xs text-gray-400 inline-block">Icons from<a href="https://icons8.com/" class="text-gray-400 font-normal" rel="noopener noreferrer">Icons8</a></span>
+      <router-link to="/legal.html#terms-of-use">Terms of Use</router-link>
+      <span class="inline-block">&ensp;&middot;&ensp;</span>
+      <router-link to="/legal.html#privacy-policy">Privacy Policy</router-link>
+      <span class="inline-block">&ensp;&middot;&ensp;</span>
+      <span class="text-xs text-gray-400 inline-block">Icons from <a href="https://icons8.com/" class="text-gray-400 font-normal" rel="noopener noreferrer" target="_blank">Icons8</a></span>
     </div>
   </div>
 </template>
